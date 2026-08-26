4c44670644b5160fd68669b68c55c9a4a54993cc
jonghyeuk <jonghyeuk@users.noreply.github.com>
2026-08-26 21:23:45 +0900

    Retarget Byeolbyeol Arcade for ages forty to sixty

--- public/game-router.js
+++ public/game-router.js
@@ -2,6 +2,7 @@ function openGameFromLink(){
   if(location.hash==='#play-excuse')openGame();
   if(location.hash==='#play-flirt')openFlirt();
   if(location.hash==='#play-friend')openFriend();
+  if(location.hash==='#play-fairy')openFairy();
 }
 window.addEventListener('hashchange',openGameFromLink);
 openGameFromLink();
