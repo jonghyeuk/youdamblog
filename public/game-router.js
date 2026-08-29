1859d82b3f070beb46977289a21ba12035296114
jonghyeuk <jonghyeuk@users.noreply.github.com>
2026-08-29 15:15:03 +0900

    Replace excuse game with ten-year life forecast

--- public/game-router.js
+++ public/game-router.js
@@ -1,5 +1,5 @@
 function openGameFromLink(){
-  if(location.hash==='#play-excuse')openGame();
+  if(location.hash==='#play-excuse')openFuture();
   if(location.hash==='#play-affair'||location.hash==='#play-flirt')openAffair();
   if(location.hash==='#play-friend')openFriend();
   if(location.hash==='#play-fairy')openFairy();
